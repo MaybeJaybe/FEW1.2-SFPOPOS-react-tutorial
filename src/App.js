1bb4f2322d1dea58189b40d43e226c9c9a162d9e
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-02-18 19:33:13 -0800

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 import Title from './Title';
 import POPOSList from './POPOSList';
+import Footer from './Footer';
 
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <img src={logo} className="App-logo" alt="logo" />
-      
       <POPOSList />
-
-
-
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="Footer">
+            Mayberry copyright 2021
+        </footer>
+    )
+}
+export default Footer
